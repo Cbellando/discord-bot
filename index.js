ba3a6ebe0c3bf170c5608cfb9ddbadb1f80011ef
Caleb Bellando <caleb.bellando@ttu.edu>
2017-10-23 13:41:01 -0500

    Read input from my text, display using /'number'

--- index.js
+++ index.js
@@ -4,22 +4,34 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log('I am ready!');
 });
+//Read in text to display
+var fs = require("fs");
+var text = fs.readFileSync("./mytext.txt", "utf-8");
+var textByLine = text.split("\n")
 
 var roll = function() {
     var result = String(Math.floor(Math.random() * 21))
+    console.log(result);
     return result
 }
 
 //Turn the discordjs on to listen to a message
     client.on('message', (message) => {
 
-//Listens to each instance of the message /knock and executes the code below
+//Listens to each instance of the message /roll and executes the code below
         if (message.content.includes('/roll')) {
             const msg = message.content.split(' ');
 
-//Function knock() returns the formatted joke
                 message.reply(roll());
 
+        }
+        else if (message.content.substr(0,1)==('/')) {
+          var num = message.content.substr(1);
+          num = parseInt(num);
+          if (!isNaN(num)){
+          console.log(num);
+          message.reply(textByLine[num]);
+          }
         }
     });
 
